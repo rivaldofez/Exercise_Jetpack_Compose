83d037c4089d8ce7dcf7bd119db453c10d8673d1
rivaldofez <rivaldofez@gmail.com>
2023-04-16 16:16:50 +0700

    add animation

--- HelloJetpackCompose/app/src/main/java/com/rivaldofez/hellojetpackcompose/MainActivity.kt
+++ HelloJetpackCompose/app/src/main/java/com/rivaldofez/hellojetpackcompose/MainActivity.kt
@@ -3,6 +3,9 @@ package com.rivaldofez.hellojetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -61,6 +64,13 @@ fun HelloJetpackComposeApp() {
 @Composable
 fun Greeting(name: String) {
     var isExpanded by remember { mutableStateOf(false) }
+    val animatedSizeDp by animateDpAsState(
+        targetValue = if (isExpanded) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Row (
         modifier = Modifier.padding(8.dp),
@@ -69,7 +79,7 @@ fun Greeting(name: String) {
         Image(
             painter = painterResource(R.drawable.jetpack_compose),
             contentDescription = "Logo Jetpack Compose",
-            modifier = Modifier.size(80.dp)
+            modifier = Modifier.size(animatedSizeDp)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column (modifier = Modifier.weight(1f)) {
