d7734428c57f2c578beffd0e44b657e6e6665ba3
rivaldofez <rivaldofez@gmail.com>
2023-04-17 10:01:51 +0700

    implement all components in main

--- JetCoffee/app/src/main/java/com/rivaldofez/jetcoffee/MainActivity.kt
+++ JetCoffee/app/src/main/java/com/rivaldofez/jetcoffee/MainActivity.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -15,9 +16,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.rivaldofez.jetcoffee.model.Menu
+import com.rivaldofez.jetcoffee.model.dummyBestSellerMenu
+import com.rivaldofez.jetcoffee.model.dummyCategory
+import com.rivaldofez.jetcoffee.model.dummyMenu
+import com.rivaldofez.jetcoffee.ui.components.CategoryItem
+import com.rivaldofez.jetcoffee.ui.components.MenuItem
 import com.rivaldofez.jetcoffee.ui.components.SearchBar
+import com.rivaldofez.jetcoffee.ui.components.SectionText
 import com.rivaldofez.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,8 +43,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
-    Column {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         Banner()
+        SectionText(title = stringResource(id = R.string.section_category))
+        CategoryRow()
+        SectionText(stringResource(R.string.section_favorite_menu))
+        MenuRow(dummyMenu)
+        SectionText(stringResource(R.string.section_best_seller_menu))
+        MenuRow(dummyBestSellerMenu)
     }
 }
 
@@ -50,8 +65,38 @@ fun Banner(
             contentScale = ContentScale.Crop,
             modifier = Modifier.height(160.dp)
         )
-
         SearchBar()
+    }
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu)
+        }
+    }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        modifier = modifier.padding(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category = category)
+        }
     }
 }
 
