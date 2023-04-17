e190dd10d532410217fc464731c0434f0eebdc78
rivaldofez <rivaldofez@gmail.com>
2023-04-17 09:35:41 +0700

    create banner

--- JetCoffee/app/src/main/java/com/rivaldofez/jetcoffee/MainActivity.kt
+++ JetCoffee/app/src/main/java/com/rivaldofez/jetcoffee/MainActivity.kt
@@ -3,13 +3,21 @@ package com.rivaldofez.jetcoffee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rivaldofez.jetcoffee.ui.components.SearchBar
 import com.rivaldofez.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,11 +34,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
-    ) {
-        Greeting("Android")
+    Column {
+        Banner()
+    }
+}
+
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+
+        SearchBar()
     }
 }
 
@@ -44,6 +65,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetCoffeeTheme {
-        Greeting("Android")
+        JetCoffeeApp()
     }
 }
