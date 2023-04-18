df017da2ddbe97821b5aed748020760326bfdda4
rivaldofez <rivaldofez@gmail.com>
2023-04-18 12:16:07 +0700

    implement lazy list

--- JetHeroes/app/src/main/java/com/rivaldofez/jetheroes/MainActivity.kt
+++ JetHeroes/app/src/main/java/com/rivaldofez/jetheroes/MainActivity.kt
@@ -16,28 +16,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetHeroesTheme {
+            JetHeroesTheme{
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    JetHeroesApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetHeroesTheme {
-        Greeting("Android")
+        JetHeroesApp()
     }
 }
