a12c4b5a68bdb0276613234bd3c60eea262a5dfd
rivaldofez <rivaldofez@gmail.com>
2023-04-22 22:53:06 +0700

    implement all scenario testing

--- JetRewardTesting/app/src/androidTest/java/com/dicoding/jetreward/JetRewardAppTest.kt
+++ JetRewardTesting/app/src/androidTest/java/com/dicoding/jetreward/JetRewardAppTest.kt
@@ -2,9 +2,11 @@ package com.dicoding.jetreward
 
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
+import com.dicoding.jetreward.model.FakeRewardDataSource
 import com.dicoding.jetreward.ui.navigation.Screen
 import com.dicoding.jetreward.ui.theme.JetRewardTheme
 import org.junit.Assert.*
@@ -16,6 +18,7 @@ class JetRewardAppTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
     private lateinit var navController: TestNavHostController
+
     @Before
     fun setUp() {
         composeTestRule.setContent {
@@ -29,8 +32,45 @@ class JetRewardAppTest {
 
     @Test
     fun navHost_verifyStartDestination() {
-//        val currentRoute = navController.currentBackStackEntry?.destination?.route
-//        assertEquals(Screen.Home.route, currentRoute)
+        navController.assertCurrentRouteName(Screen.Home.route)
+    }
+
+    @Test
+    fun navHost_clickItem_navigatesToDetailWithData() {
+        composeTestRule.onNodeWithTag("RewardList").performScrollToIndex(10)
+        composeTestRule.onNodeWithText(FakeRewardDataSource.dummyRewards[10].title).performClick()
+        navController.assertCurrentRouteName(Screen.DetailReward.route)
+        composeTestRule.onNodeWithText(FakeRewardDataSource.dummyRewards[10].title).assertIsDisplayed()
+    }
+
+    @Test
+    fun navHost_bottomNavigation_working() {
+        composeTestRule.onNodeWithStringId(R.string.menu_cart).performClick()
+        navController.assertCurrentRouteName(Screen.Cart.route)
+        composeTestRule.onNodeWithStringId(R.string.menu_profile).performClick()
+        navController.assertCurrentRouteName(Screen.Profile.route)
+        composeTestRule.onNodeWithStringId(R.string.menu_home).performClick()
+        navController.assertCurrentRouteName(Screen.Home.route)
+    }
+
+    @Test
+    fun navHost_clickItem_navigatesBack() {
+        composeTestRule.onNodeWithTag("RewardList").performScrollToIndex(10)
+        composeTestRule.onNodeWithText(FakeRewardDataSource.dummyRewards[10].title).performClick()
+        navController.assertCurrentRouteName(Screen.DetailReward.route)
+        composeTestRule.onNodeWithContentDescription(composeTestRule.activity.getString(R.string.back)).performClick()
+        navController.assertCurrentRouteName(Screen.Home.route)
+    }
+
+    @Test
+    fun navHost_checkout_rightBackStack() {
+        composeTestRule.onNodeWithText(FakeRewardDataSource.dummyRewards[4].title).performClick()
+        navController.assertCurrentRouteName(Screen.DetailReward.route)
+        composeTestRule.onNodeWithStringId(R.string.plus_symbol).performClick()
+        composeTestRule.onNodeWithContentDescription("Order Button").performClick()
+        navController.assertCurrentRouteName(Screen.Cart.route)
+        composeTestRule.onNodeWithStringId(R.string.menu_home).performClick()
         navController.assertCurrentRouteName(Screen.Home.route)
     }
 }
+
