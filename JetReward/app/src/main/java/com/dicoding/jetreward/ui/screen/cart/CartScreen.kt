4add3d76ffe11b5c7c50ab2604062232329874da
rivaldofez <rivaldofez@gmail.com>
2023-04-22 21:19:14 +0700

    implement intent to other apps

--- JetReward/app/src/main/java/com/dicoding/jetreward/ui/screen/cart/CartScreen.kt
+++ JetReward/app/src/main/java/com/dicoding/jetreward/ui/screen/cart/CartScreen.kt
@@ -31,7 +31,8 @@ fun CartScreen(
             Injection.provideRepository()
         )
     ),
-) {
+    onOrderButtonClicked: (String) -> Unit,
+    ) {
     viewModel.uiState.collectAsState(initial = UiState.Loading).value.let { uiState ->
         when (uiState) {
             is UiState.Loading -> {
@@ -43,6 +44,7 @@ fun CartScreen(
                     onProductCountChanged = { rewardId, count ->
                         viewModel.updateOrderReward(rewardId, count)
                     },
+                    onOrderButtonClicked = onOrderButtonClicked
                 )
             }
             is UiState.Error -> {}
@@ -53,8 +55,15 @@ fun CartScreen(
 fun CartContent(
     state: CartState,
     onProductCountChanged: (id: Long, count: Int) -> Unit,
+    onOrderButtonClicked: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val shareMessage = stringResource(
+        R.string.share_message,
+        state.orderReward.count(),
+        state.totalRequiredPoint
+    )
+
     Column(
         modifier = modifier.fillMaxSize()
     ) {
@@ -70,7 +79,9 @@ fun CartContent(
         OrderButton(
             text = stringResource(R.string.total_order, state.totalRequiredPoint),
             enabled = state.orderReward.isNotEmpty(),
-            onClick = {},
+            onClick = {
+                onOrderButtonClicked(shareMessage)
+            },
             modifier = Modifier.padding(16.dp)
         )
         LazyColumn(
